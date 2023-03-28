5c9b2c92f990877b7d980db66ef4d1d5a817ffde
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2023-03-29 00:20:29 +0600

    Initial Commit

---
+++ FriendsApp/FriendsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsAppApp.swift
+//  FriendsApp
+//
+//  Created by Shakhawat Hossain Shahin on 29/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
